a807a474972c595075c6e805624c1ca3e424a0e2
Hyunsoo Park <zahir84@nate.com>
2023-11-23 11:23:55 +0900

    초기 커밋.

---
+++ MsSqlManagerLibrary/HostConnection.cs
@@ -0,0 +1,559 @@
+﻿using System.Collections.Generic;
+using System.Data;
+using System.Data.OleDb;
+
+namespace MsSqlManagerLibrary
+{
+    public class HostConnection
+    {
+        public static MsSqlManager msSql = null;
+        public static bool bConnection = false;
+
+        public static string Connect()
+        {
+            if (msSql != null)
+                return "NG";
+
+            string strConnetion = string.Format("server=10.131.15.18;database=EE;user id=eeuser;password=Amkor123!");
+
+            msSql = new MsSqlManager(strConnetion);
+
+            if (msSql.OpenTest() == false)
+            {
+                bConnection = false;
+                return "NG";
+            }
+            else
+            {
+                bConnection = true;
+            }
+            
+            return "OK";
+        }
+
+        #region SystemStatus
+        public static string Host_Get_SystemStatus(string EquipmentName, string module)
+        {
+            string query = "";
+            query = string.Format(@"SELECT * FROM {0}", EquipmentName);
+
+            DataTable dT = msSql.GetData(query);
+
+            if (dT.Rows.Count < 1)
+                return null;
+
+            string rtnStr = dT.Rows[0][string.Format("{0}_Status", module)].ToString();
+
+            return rtnStr;
+        }
+
+        public static void Host_Set_SystemStatus(string EquipmentName, string module, string setStr)
+        {
+            setStr = setStr.Trim();
+
+            // 지우고 추가
+            List<string> queryList = new List<string>();
+
+            Host_Delete_SystemStatus(EquipmentName, module);
+
+            //string query = string.Format(@"INSERT INTO {0} ({1}_Status) VALUES ('{2}')", EquipmentName, module, setStr);
+            string query = string.Format("UPDATE {0} SET {1}_Status = '{2}'", EquipmentName, module, setStr);
+            queryList.Add(query);
+
+            msSql.SetData(queryList);
+        }
+
+        public static int Host_Delete_SystemStatus(string EquipmentName, string module)
+        {
+            string query = string.Format("DELETE FROM {0} WHERE {1}_Status", EquipmentName, module);
+
+            int n = msSql.SetData(query);
+
+            return n;
+        }
+        #endregion
+
+        #region Module RunStatus
+        public static string Host_Get_RunStatus(string EquipmentName, string module)
+        {
+            string query = "";
+            query = string.Format(@"SELECT * FROM {0}", EquipmentName);
+
+            DataTable dT = msSql.GetData(query);
+
+            if (dT.Rows.Count < 1)
+                return null;
+
+            string rtnStr = dT.Rows[0][string.Format("{0}_RunStatus", module)].ToString();            
+
+            return rtnStr;
+        }
+
+        public static void Host_Set_RunStatus(string EquipmentName, string module, string setStr)
+        {
+            setStr = setStr.Trim();
+
+            // 지우고 추가
+            List<string> queryList = new List<string>();
+
+            Host_Delete_RunStatus(EquipmentName, module);
+
+            //string query = string.Format(@"INSERT INTO {0} ({1}_RunStatus) VALUES ('{2}')", EquipmentName, module, setStr);
+            string query = string.Format("UPDATE {0} SET {1}_RunStatus = '{2}'", EquipmentName, module, setStr);
+            queryList.Add(query);
+
+            msSql.SetData(queryList);
+        }
+
+        public static int Host_Delete_RunStatus(string EquipmentName, string module)
+        {
+            string query = string.Format("DELETE FROM {0} WHERE {1}_RunStatus", EquipmentName, module);
+
+            int n = msSql.SetData(query);
+
+            return n;
+        }
+        #endregion
+
+        #region RecipeName
+        public static string Host_Get_RecipeName(string EquipmentName, string module)
+        {
+            string query = "";
+            query = string.Format(@"SELECT * FROM {0}", EquipmentName);
+
+            DataTable dT = msSql.GetData(query);
+
+            if (dT.Rows.Count < 1)
+                return null;
+
+            string rtnStr = dT.Rows[0][string.Format("{0}_RecipeName", module)].ToString();
+
+            return rtnStr;
+        }
+
+        public static void Host_Set_RecipeName(string EquipmentName, string module, string setStr)
+        {
+            setStr = setStr.Trim();
+
+            // 지우고 추가
+            List<string> queryList = new List<string>();
+
+            Host_Delete_RecipeName(EquipmentName, module);
+
+            //string query = string.Format(@"INSERT INTO {0} ({1}_RecipeName) VALUES ('{2}')", EquipmentName, module, setStr);
+            string query = string.Format("UPDATE {0} SET {1}_RecipeName = '{2}'", EquipmentName, module, setStr);
+            queryList.Add(query);
+
+            msSql.SetData(queryList);
+        }
+
+        public static int Host_Delete_RecipeName(string EquipmentName, string module)
+        {
+            string query = string.Format("DELETE FROM {0} WHERE {1}_RecipeName", EquipmentName, module);
+
+            int n = msSql.SetData(query);
+
+            return n;
+        }
+        #endregion
+
+        #region AlarmName
+        public static string Host_Get_AlarmName(string EquipmentName, string module)
+        {
+            string query = "";
+            query = string.Format(@"SELECT * FROM {0}", EquipmentName);
+
+            DataTable dT = msSql.GetData(query);
+
+            if (dT.Rows.Count < 1)
+                return null;
+
+            string rtnStr = dT.Rows[0][string.Format("{0}_AlarmName", module)].ToString();
+
+            return rtnStr;
+        }
+
+        public static void Host_Set_AlarmName(string EquipmentName, string module, string setStr)
+        {
+            setStr = setStr.Trim();
+
+            // 지우고 추가
+            List<string> queryList = new List<string>();
+
+            Host_Delete_AlarmName(EquipmentName, module);
+
+            //string query = string.Format(@"INSERT INTO {0} ({1}_AlarmName) VALUES ('{2}')", EquipmentName, module, setStr);
+            string query = string.Format("UPDATE {0} SET {1}_AlarmName = '{2}'", EquipmentName, module, setStr);
+            queryList.Add(query);
+
+            msSql.SetData(queryList);
+        }
+
+        public static int Host_Delete_AlarmName(string EquipmentName, string module)
+        {
+            string query = string.Format("DELETE FROM {0} WHERE {1}_AlarmName", EquipmentName, module);
+
+            int n = msSql.SetData(query);
+
+            return n;
+        }
+        #endregion
+
+        # region DailyCount
+        public static string Host_Get_DailyCount(string EquipmentName, string module)
+        {
+            string query = "";
+            query = string.Format(@"SELECT * FROM {0}", EquipmentName);
+
+            DataTable dT = msSql.GetData(query);
+
+            if (dT.Rows.Count < 1)
+                return null;
+
+            string rtnStr = dT.Rows[0][string.Format("{0}_DailyCount", module)].ToString();
+
+            return rtnStr;
+        }
+
+        public static void Host_Set_DailyCount(string EquipmentName, string module, string setStr)
+        {
+            setStr = setStr.Trim();
+
+            // 지우고 추가
+            List<string> queryList = new List<string>();
+
+            Host_Delete_DailyCount(EquipmentName, module);
+
+            //string query = string.Format(@"INSERT INTO {0} ({1}_DailyCount) VALUES ('{2}')", EquipmentName, module, setStr);
+            string query = string.Format("UPDATE {0} SET {1}_DailyCount = '{2}'", EquipmentName, module, setStr);
+            queryList.Add(query);
+
+            msSql.SetData(queryList);
+        }
+
+        public static int Host_Delete_DailyCount(string EquipmentName, string module)
+        {
+            string query = string.Format("DELETE FROM {0} WHERE {1}_DailyCount", EquipmentName, module);
+
+            int n = msSql.SetData(query);
+
+            return n;
+        }
+        #endregion
+
+        #region Progress Time
+        public static string Host_Get_ProgressTime(string EquipmentName, string module)
+        {
+            string query = "";
+            query = string.Format(@"SELECT * FROM {0}", EquipmentName);
+
+            DataTable dT = msSql.GetData(query);
+
+            if (dT.Rows.Count < 1)
+                return null;
+
+            string rtnStr = dT.Rows[0][string.Format("{0}_ProgressTime", module)].ToString();
+
+            return rtnStr;
+        }
+
+        public static void Host_Set_ProgressTime(string EquipmentName, string module, string setStr)
+        {
+            setStr = setStr.Trim();
+
+            // 지우고 추가
+            List<string> queryList = new List<string>();
+
+            Host_Delete_ProgressTime(EquipmentName, module);
+
+            //string query = string.Format(@"INSERT INTO {0} ({1}_ProgressTime) VALUES ('{2}')", EquipmentName, module, setStr);
+            string query = string.Format("UPDATE {0} SET {1}_ProgressTime = '{2}'", EquipmentName, module, setStr);
+            queryList.Add(query);
+
+            msSql.SetData(queryList);
+        }
+
+        public static int Host_Delete_ProgressTime(string EquipmentName, string module)
+        {
+            string query = string.Format("DELETE FROM {0} WHERE {1}_ProgressTime", EquipmentName, module);
+
+            int n = msSql.SetData(query);
+
+            return n;
+        }
+        #endregion
+
+        #region Process End Time
+        public static string Host_Get_ProcessEndTime(string EquipmentName, string module)
+        {
+            string query = "";
+            query = string.Format(@"SELECT * FROM {0}", EquipmentName);
+
+            DataTable dT = msSql.GetData(query);
+
+            if (dT.Rows.Count < 1)
+                return null;
+
+            string rtnStr = dT.Rows[0][string.Format("{0}_ProcessEndTime", module)].ToString();
+
+            return rtnStr;
+        }
+
+        public static void Host_Set_ProcessEndTime(string EquipmentName, string module, string setStr)
+        {
+            setStr = setStr.Trim();
+
+            // 지우고 추가
+            List<string> queryList = new List<string>();
+
+            Host_Delete_ProcessEndTime(EquipmentName, module);
+
+            //string query = string.Format(@"INSERT INTO {0} ({1}_ProcessEndTime) VALUES ('{2}')", EquipmentName, module, setStr);
+            string query = string.Format("UPDATE {0} SET {1}_ProcessEndTime = '{2}'", EquipmentName, module, setStr);
+            queryList.Add(query);
+
+            msSql.SetData(queryList);
+        }
+
+        public static int Host_Delete_ProcessEndTime(string EquipmentName, string module)
+        {
+            string query = string.Format("DELETE FROM {0} WHERE {1}_ProcessEndTime", EquipmentName, module);
+
+            int n = msSql.SetData(query);
+
+            return n;
+        }
+        #endregion
+
+        #region Tool Sensor1
+        public static string Host_Get_ToolSensor1(string EquipmentName, string module)
+        {
+            string query = "";
+            query = string.Format(@"SELECT * FROM {0}", EquipmentName);
+
+            DataTable dT = msSql.GetData(query);
+
+            if (dT.Rows.Count < 1)
+                return null;
+
+            string rtnStr = dT.Rows[0][string.Format("{0}_ToolSensor1", module)].ToString();
+
+            return rtnStr;
+        }
+
+        public static void Host_Set_ToolSensor1(string EquipmentName, string module, string setStr)
+        {
+            setStr = setStr.Trim();
+
+            // 지우고 추가
+            List<string> queryList = new List<string>();
+
+            Host_Delete_ToolSensor1(EquipmentName, module);
+
+            //string query = string.Format(@"INSERT INTO {0} ({1}_ToolSensor1) VALUES ('{2}')", EquipmentName, module, setStr);
+            string query = string.Format("UPDATE {0} SET {1}_ToolSensor1 = '{2}'", EquipmentName, module, setStr);
+            queryList.Add(query);
+
+            msSql.SetData(queryList);
+        }
+
+        public static int Host_Delete_ToolSensor1(string EquipmentName, string module)
+        {
+            string query = string.Format("DELETE FROM {0} WHERE {1}_ToolSensor1", EquipmentName, module);
+
+            int n = msSql.SetData(query);
+
+            return n;
+        }
+        #endregion
+
+        #region Tool Sensor2
+        public static string Host_Get_ToolSensor2(string EquipmentName, string module)
+        {
+            string query = "";
+            query = string.Format(@"SELECT * FROM {0}", EquipmentName);
+
+            DataTable dT = msSql.GetData(query);
+
+            if (dT.Rows.Count < 1)
+                return null;
+
+            string rtnStr = dT.Rows[0][string.Format("{0}_ToolSensor2", module)].ToString();
+
+            return rtnStr;
+        }
+
+        public static void Host_Set_ToolSensor2(string EquipmentName, string module, string setStr)
+        {
+            setStr = setStr.Trim();
+
+            // 지우고 추가
+            List<string> queryList = new List<string>();
+
+            Host_Delete_ToolSensor2(EquipmentName, module);
+
+            //string query = string.Format(@"INSERT INTO {0} ({1}_ToolSensor2) VALUES ('{2}')", EquipmentName, module, setStr);
+            string query = string.Format("UPDATE {0} SET {1}_ToolSensor2 = '{2}'", EquipmentName, module, setStr);
+            queryList.Add(query);
+
+            msSql.SetData(queryList);
+        }
+
+        public static int Host_Delete_ToolSensor2(string EquipmentName, string module)
+        {
+            string query = string.Format("DELETE FROM {0} WHERE {1}_ToolSensor2", EquipmentName, module);
+
+            int n = msSql.SetData(query);
+
+            return n;
+        }
+        #endregion
+
+        # region UPH
+        public static string Host_Get_UPH(string EquipmentName)
+        {
+            string query = "";
+            query = string.Format(@"SELECT * FROM {0}", EquipmentName);
+
+            DataTable dT = msSql.GetData(query);
+
+            if (dT.Rows.Count < 1)
+                return null;
+
+            string rtnStr = dT.Rows[0][string.Format("UPH")].ToString();
+
+            return rtnStr;
+        }
+
+        public static void Host_Set_UPH(string EquipmentName, string setStr)
+        {
+            setStr = setStr.Trim();
+
+            // 지우고 추가
+            List<string> queryList = new List<string>();
+
+            Host_Delete_UPH(EquipmentName);
+            
+            string query = string.Format("UPDATE {0} SET UPH = '{1}'", EquipmentName, setStr);
+            queryList.Add(query);
+
+            msSql.SetData(queryList);
+        }
+
+        public static int Host_Delete_UPH(string EquipmentName)
+        {
+            string query = string.Format("DELETE FROM {0} WHERE UPH", EquipmentName);
+
+            int n = msSql.SetData(query);
+
+            return n;
+        }
+        #endregion
+
+        #region LOG
+        public static string[] Host_Get_Log(string EquipmentName, string dateTime)
+        {            
+            string[] rtnStr = new string[7];
+
+            string dbConnetion = string.Format("Provider=SQLOLEDB;server=10.131.15.18;database=EE;user id=eeuser;password=Amkor123!");
+
+            using (OleDbConnection dbConn = new OleDbConnection(dbConnetion))
+            {
+                dbConn.Open();
+
+                try
+                {                    
+                    string query = string.Format(@"SELECT * FROM {0} WHERE Time = '{1}'", EquipmentName, dateTime);
+                    OleDbCommand cmd = new OleDbCommand(query, dbConn);
+                    OleDbDataReader reader;
+                    reader = cmd.ExecuteReader();
+
+                    while (true)
+                    {
+                        if (reader.Read())
+                        {
+                            rtnStr[0] = reader.GetValue(0).ToString().Trim();   // DateTime
+                            rtnStr[1] = reader.GetValue(1).ToString().Trim();   // CH1 daily count
+                            rtnStr[2] = reader.GetValue(2).ToString().Trim();   // CH2 daily count
+                            rtnStr[3] = reader.GetValue(3).ToString().Trim();   // CH3 daily count
+                            rtnStr[4] = reader.GetValue(4).ToString().Trim();   // Performance (Only daily count)
+                            rtnStr[5] = reader.GetValue(5).ToString().Trim();   // Today run time
+                            rtnStr[6] = reader.GetValue(6).ToString().Trim();   // Performance (실제 가동 시간 기준)
+                            break;
+                        }
+                        else
+                        {
+                            rtnStr[0] = "";
+                        }
+                    }
+                    
+                    return rtnStr;
+                }
+                catch
+                {                    
+                    rtnStr[0] = "";
+                    return rtnStr;
+                }
+                finally
+                {
+                    dbConn.Close();
+                }            
+            }     
+        }
+
+        public static void Host_Set_Log(string EquipmentName, string dateTime, string module1Value, string module2Value, string module3Value, string setStr1, string setStr2, string setStr3)
+        {
+            setStr1 = setStr1.Trim();
+            setStr2 = setStr2.Trim();
+
+            List<string> queryList = new List<string>();            
+            
+            string query = string.Format(@"INSERT INTO {0} VALUES ('{1}', '{2}', '{3}', '{4}', '{5}', '{6}', '{7}')", EquipmentName, dateTime, module1Value, module2Value, module3Value, setStr1, setStr2, setStr3);            
+            queryList.Add(query);
+
+            msSql.SetData(queryList);
+        }
+        #endregion
+
+        #region Freezer&Refrigerator temp monitoring
+        public static string Host_Get_TempHumi(string EquipmentName, int PiNo, string Item)
+        {
+            string query = "";
+            query = string.Format(@"SELECT * FROM {0}", EquipmentName);
+
+            DataTable dT = msSql.GetData(query);
+
+            if (dT.Rows.Count < 1)
+                return null;
+
+            string rtnStr = dT.Rows[PiNo-1][string.Format("{0}", Item)].ToString();
+
+            return rtnStr;
+        }
+
+        public static void Host_Set_TempHumi(string EquipmentName, string PiNo, string Item, string setStr)
+        {
+            setStr = setStr.Trim();
+
+            // 지우고 추가
+            List<string> queryList = new List<string>();
+
+            Host_Delete_TempHumi(EquipmentName, PiNo, Item);
+            
+            string query = string.Format("UPDATE {0} SET {1} = '{2}' WHERE PiNo = {3}", EquipmentName, Item, setStr, PiNo);
+            queryList.Add(query);
+
+            msSql.SetData(queryList);
+        }
+
+        public static int Host_Delete_TempHumi(string EquipmentName, string PiNo, string Item)
+        {
+            string query = string.Format("DELETE FROM {0} {1} WHERE {2}", EquipmentName, Item, PiNo);
+
+            int n = msSql.SetData(query);
+
+            return n;
+        }
+        #endregion
+    }
+}
---
+++ MsSqlManagerLibrary/MsSqlManager.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Threading;
+
+namespace MsSqlManagerLibrary
+{
+    public class MsSqlManager
+    {
+        private string sqlConnectionString;
+        int GetRetryMax = 5;
+        int RetryMax = 5;
+        
+        public string ParameterServerIP { get; set; }
+
+        public string ParameterDatabaseName { get; set; }
+
+        public string ParameterUserId { get; set; }
+
+        public string ParameterPassword { get; set; }
+
+        public string DatabasePath { get; set; }
+
+        // Database가 존재 하지 않을 경우 처리를 위하여 Datasource를 파싱 및 재 조합
+        public MsSqlManager(string sqlConnectionString)
+        {
+            if (sqlConnectionString.Substring(0, 1) == "D")
+            {
+
+            }
+            else
+            {
+                ParameterServerIP = sqlConnectionString.Split(';')[0].Split('=')[1].ToString();
+                ParameterDatabaseName = sqlConnectionString.Split(';')[1].Split('=')[1].ToString();
+                ParameterUserId = sqlConnectionString.Split(';')[2].Split('=')[1].ToString();
+                ParameterPassword = sqlConnectionString.Split(';')[3].Split('=')[1].ToString();
+
+            }            
+
+            this.sqlConnectionString = sqlConnectionString;
+        }
+
+        public bool OpenTest(bool first = true)
+        {
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(sqlConnectionString))
+                {
+                    connection.Open();
+                }
+
+                return true;
+            }            
+            catch (Exception)
+            {                
+                //
+            }
+
+            return false;
+        }
+
+        public DataTable GetData(string queryString, int retry = 1)
+        {
+            DataTable dT = new DataTable();
+
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(sqlConnectionString))
+                {
+                    connection.Open();
+
+                    using (SqlCommand cmd = new SqlCommand(queryString, connection))
+                    {
+                        using (SqlDataAdapter dataAdapter = new SqlDataAdapter(cmd))
+                        {
+                            dataAdapter.Fill(dT);
+                        }
+                    }
+                }
+            }
+            catch (Exception)
+            {
+                if (retry == GetRetryMax)
+                {
+                    
+                }
+                else
+                {
+                    Thread.Sleep(100);                    
+                    return GetData(queryString, ++retry);
+                }
+            }
+            return dT;
+        }
+
+        public int SetData(string queryString, SqlParameter[] sqlParam = null, CommandType cmdType = CommandType.Text, int retry = 1)
+        {
+            int ret = 0;
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(sqlConnectionString))
+                {
+                    connection.Open();
+                    using (SqlCommand cmd = new SqlCommand(queryString, connection))
+                    {
+                        if (sqlParam != null)
+                            cmd.Parameters.AddRange(sqlParam);
+
+                        cmd.CommandType = cmdType;
+                        cmd.CommandTimeout = 300;
+
+                        ret = cmd.ExecuteNonQuery();
+                    }
+                }
+            }
+            catch (SqlException)
+            {
+                if (retry == RetryMax)
+                {                    
+                    return ret;
+                }
+                else
+                {
+                    Thread.Sleep(100);                    
+                    return SetData(queryString, sqlParam, cmdType, ++retry);
+                }
+            }
+            return ret;
+        }
+
+        public int SetData(List<string> queryList, CommandType cmdType = CommandType.Text, int retry = 1)
+        {
+            if (queryList.Count == 0)
+                return 0;
+
+            int ret = 0;
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(sqlConnectionString))
+                {
+                    connection.Open();
+                    SqlTransaction transaction = connection.BeginTransaction();
+
+                    foreach (var query in queryList)
+                    {
+                        using (SqlCommand cmd = new SqlCommand(query, connection))
+                        {
+                            cmd.CommandType = cmdType;
+                            cmd.Transaction = transaction;
+                            ret = cmd.ExecuteNonQuery();
+                        }
+                    }
+                    transaction.Commit();                    
+                }
+            }
+            catch (SqlException)
+            {
+                if (retry == RetryMax)
+                {                    
+                    return ret;
+                }
+                else
+                {
+                    Thread.Sleep(100);                    
+                    return SetData(queryList, cmdType, ++retry);
+                }
+            }
+            return ret;
+        }        
+    }
+}
